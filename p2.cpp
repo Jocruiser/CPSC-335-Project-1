f513f5da5a5a51ac8bf9b82a0b1f7f3855767cc1
andresga11 <69830280+andresga11@users.noreply.github.com>
2023-02-26 22:28:32 -0800

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -1,16 +1,8 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-
-// helper function to convert time string to minutes since midnight
-int timeToMinutes(string timeStr) {
-    int hours = stoi(timeStr.substr(0, 2));
-    int minutes = stoi(timeStr.substr(3, 2));
-    return hours * 60 + minutes;
-}
 
 // helper function to convert minutes since midnight to time string
 string minutesToTime(int minutes) {
@@ -19,13 +11,22 @@ string minutesToTime(int minutes) {
     return (hours < 10 ? "0" : "") + to_string(hours) + ":" + (mins < 10 ? "0" : "") + to_string(mins);
 }
 
+// helper function to convert time string to minutes since midnight
+int timeToMinutes(string timeStr) {
+    int hours = stoi(timeStr.substr(0, 2));
+    int minutes = stoi(timeStr.substr(3, 2));
+    return hours * 60 + minutes;
+}
 
+// function for finding common availbility time between both people
 vector<pair<int, int>> findAvailableTimes(vector<vector<string>> person1_Schedule, vector<string> person1_DailyAct, 
                                                 vector<vector<string>> person2_Schedule, vector<string> person2_DailyAct, 
                                                 int duration) 
 {
-    vector<pair<int, int>> unavailableTimes; // combine schedules and daily activities into one list of unavailable times
+    // combine schedules and daily activities into one list of unavailable times
+    vector<pair<int, int>> unavailableTimes; 
     
+   
     for (auto& slot : person1_Schedule) {
         unavailableTimes.emplace_back(timeToMinutes(slot[0]), timeToMinutes(slot[1]));
     }
@@ -33,27 +34,31 @@ vector<pair<int, int>> findAvailableTimes(vector<vector<string>> person1_Schedul
         unavailableTimes.emplace_back(timeToMinutes(slot[0]), timeToMinutes(slot[1]));
     }
     
+    //sorting in increasing order
     sort(unavailableTimes.begin(), unavailableTimes.end());
-    
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+
     vector<pair<int,int>> availableTimes;
     
     int begin_time = max(timeToMinutes(person1_DailyAct[0]),timeToMinutes(person2_DailyAct[0]));
     int block_end_time = min(timeToMinutes(person1_DailyAct[1]),timeToMinutes(person2_DailyAct[1]));
-    int prevous_end_time = block_end_time;
+    int prevous_end_time = begin_time;
     
+    //finding available times using unavailableTimes array
     for(auto& slot: unavailableTimes){
+        //if overlap between the schedules is found skip to the next iteration
         if(slot.first < begin_time || slot.second > block_end_time){
             continue;
         }
         
+        //If there is an open slot between the previous slot and the current slot then add open slot to availableTimes
         else if(slot.first - prevous_end_time >= duration){
             availableTimes.push_back({prevous_end_time, slot.first});
         }
         
+        //updates the prevous_end_time
         prevous_end_time = max(prevous_end_time, slot.second);
     }
-    
+    //add an availble time slot for the gap between the end of the last schedule to the end of the dailyAct time.
     if(block_end_time - prevous_end_time >= duration){
         availableTimes.push_back({prevous_end_time, block_end_time});
     }
